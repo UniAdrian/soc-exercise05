b1b79008a092df7afefbb8329d9c6f3f460505d9
UniAdrian <uk077035@student.uni-kassel.de>
2021-01-20 20:21:18 +0100

    Added AllArgsConstructor to ProductServiceImpl so we can actually test it. Fixes #1

--- src/main/java/de/unikassel/soc/platform/services/ProductServiceImpl.java
+++ src/main/java/de/unikassel/soc/platform/services/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import de.unikassel.soc.platform.repos.ProductRepo;
 import de.unikassel.soc.platform.web.mappers.ProductMapper;
 import de.unikassel.soc.platform.web.mappers.ProductMapperImpl;
 import de.unikassel.soc.platform.web.model.ProductDto;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
