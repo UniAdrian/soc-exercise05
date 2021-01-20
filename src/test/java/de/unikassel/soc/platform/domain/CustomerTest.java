0cd18cc36a1bb541eef81fd6d43869ab2527cf95
UniAdrian <uk077035@student.uni-kassel.de>
2021-01-20 17:54:32 +0100

    Added getProducts Test to CustomerTest.java

--- src/test/java/de/unikassel/soc/platform/domain/CustomerTest.java
+++ src/test/java/de/unikassel/soc/platform/domain/CustomerTest.java
@@ -3,6 +3,10 @@ package de.unikassel.soc.platform.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
@@ -20,4 +24,13 @@ class CustomerTest {
         assertEquals("Test", customer.getName());
     }
 
+    @Test
+    void getProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product.ProductBuilder().productName("Product 1").currency("Euro").description("Test product 1").price(10.5d).build());
+        products.add(new Product.ProductBuilder().productName("Product 2").currency("Pound").description("Test product 2").price(1.25d).build());
+        customer.setProducts(products);
+
+        assertEquals(products, customer.getProducts());
+    }
 }
