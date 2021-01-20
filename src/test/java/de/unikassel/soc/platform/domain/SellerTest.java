db904384c8ba75866bf0f44d3cdd9889d043c3b3
UniAdrian <uk077035@student.uni-kassel.de>
2021-01-20 18:11:06 +0100

    Added Tests for Seller class.

---
+++ src/test/java/de/unikassel/soc/platform/domain/SellerTest.java
@@ -0,0 +1,22 @@
+package de.unikassel.soc.platform.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerTest {
+
+    Seller seller;
+
+    @BeforeEach
+    void setUp() {
+        seller = new Seller();
+    }
+
+    @Test
+    void getName() {
+        seller.setName("Test Name");
+        assertEquals("Test Name", seller.getName());
+    }
+}
